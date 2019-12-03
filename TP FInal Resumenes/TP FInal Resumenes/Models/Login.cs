b3d937daac340d0b70d604ddc057594c6859a3d8
GAEL SALOMON COHEN <44870316@comunidadort.edu.ar>
2019-12-03 12:29:15 -0300

    sadsadsa

--- TP FInal Resumenes/TP FInal Resumenes/Models/Login.cs
+++ TP FInal Resumenes/TP FInal Resumenes/Models/Login.cs
@@ -8,10 +8,10 @@ namespace TP_FInal_Resumenes.Models
 {
     public class Login
     {
-        [Required(ErrorMessage = "Ingrese un Username")]
+        [Required(ErrorMessage = "Nombre de usuario incorrecto")]
         public string Username { get; set; }
         [Required(ErrorMessage = "Ingrese una contraseña")]
-        [StringLength(15, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre 6 y 15 caracteres")]
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "Contraseña incorrecta")]
         public string Contrasena { get; set; }
     }
 }
